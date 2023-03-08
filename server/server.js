dab514e28a353d0cdf693422dbd0ebc103ad4c07
Jason Lieb <Jason.lieb@outlook.com>
2023-03-08 11:15:45 -0500

    run prettier

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require('express')
+const path = require('path')
+const db = require('./config/connection')
+const routes = require('./routes')
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+require('dotenv').config()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express()
+const PORT = process.env.PORT || 5500
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  console.log('running production')
+  app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
-app.use(routes);
+app.use(routes)
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
+})
